bbabd5a0f767f4115481c91534e83ee08a46f6f9
Büşra Yılmaz <124158606+Busraylmzz1@users.noreply.github.com>
2025-03-18 02:04:57 +0300

    new
    new

--- src/main/java/com/enoca/spring/e_commerce/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enoca.spring.e_commerce.repository;
-import com.enoca.spring.e_commerce.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
